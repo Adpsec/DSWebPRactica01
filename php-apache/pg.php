bff187de4f0fd57496867cdef24a06941391cdbc
Adpsec <asp.security@protonmail.com>
2023-08-29 12:23:40 -0600

    Subiendo ph.php

--- php-apache/pg.php
+++ php-apache/pg.php
@@ -1,9 +1,9 @@
 <?php
-$host = "nombre_del_host"; // Cambia esto al nombre o dirección IP de tu servidor PostgreSQL
-$port = "puerto"; // Cambia esto al puerto en el que PostgreSQL está escuchando (por lo general es 5432)
-$dbname = "nombre_de_la_base_de_datos"; // Cambia esto al nombre de tu base de datos
-$user = "usuario"; // Cambia esto al nombre de usuario de PostgreSQL
-$password = "contraseña"; // Cambia esto a la contraseña del usuario
+$host = "172.17.0.2"; // Cambia esto al nombre o dirección IP de tu servidor PostgreSQL
+$port = "5433"; // Cambia esto al puerto en el que PostgreSQL está escuchando (por lo general es 5432)
+$dbname = "ejemplo"; // Cambia esto al nombre de tu base de datos
+$user = "postgres"; // Cambia esto al nombre de usuario de PostgreSQL
+$password = "postgres"; // Cambia esto a la contraseña del usuario
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
