01dbc5935f67b896d10c9a06ff58ef9a177a15a2
jairH3110 <chiefeon3110@gmail.com>
2023-09-07 21:41:44 -0600

    añadiendo front y conexion

--- crud-apache-php/src/conexion.php
+++ crud-apache-php/src/conexion.php
@@ -1,4 +1,16 @@
 
 
+<?php
+$host = "172.17.0.2";
+$port = "5432";
+$dbname = "mydb";
+$user = "postgres";
+$password = "postgres";
 
-
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    throw new Exception("Error al conectar a la base de datos: " . $e->getMessage());
+}
+?>
